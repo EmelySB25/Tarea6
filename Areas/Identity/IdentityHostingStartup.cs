281df3ff69b7432659704dd2f35de06a55f3615d
emely <emely@example.com>
2020-11-12 01:08:05 -0600

    commit initial

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tarea6.Data;
+
+[assembly: HostingStartup(typeof(Tarea6.Areas.Identity.IdentityHostingStartup))]
+namespace Tarea6.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tarea6.Data
+{
+    
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public DbSet<AppUser> AppUsers {get; set;}
+        public DbSet<Tesoros> Treasures {get; set;}
+        public DbSet<Coordinate> Coordinates {get; protected set;}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<AppUser>(entityTypeBuilder => {
+                entityTypeBuilder.ToTable("Users");
+                entityTypeBuilder.Property(u => u.Uid).HasMaxLength(100).IsRequired();
+                entityTypeBuilder.Property(u => u.Name).HasMaxLength(100);
+                entityTypeBuilder.Property( u => u.Color).HasMaxLength(8).HasDefaultValue("#ffffff");
+            });
+        }
+    }
+    
+
+
+
+    public class AppUser : IdentityUser
+    {
+        public Guid Uid {get; set;}
+        public string Name {get; set;}
+        public string Color {get; set;}
+    }
+    public class Tesoros
+    {
+        public Guid Uid {get; set;}
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public DateTime Date {get; set;}
+        public float Weigth {get; set;}
+        public string Place {get; set;}
+        public float Value {get; set;}
+        public Coordinate Coordinates {get; set;}
+        public string UrlRef {get; set;}
+    }
+    public class Coordinate 
+    {
+        public Guid Id {get; set;}
+        public Guid Tid {get; set;}
+        public Guid Uid {get; set;}
+        public float Lat {get; set;}
+        public float Lng {get; set;}
+    }
+
+}
